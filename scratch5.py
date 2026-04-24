cc9c03be0ad9ccba5acf000f1534dc203a70e26e
Dinoop <dinoopm@gmail.com>
2026-04-24 18:59:15 +0200

    chore: remove one-off scratch files and ignore them in .gitignore

--- scratch5.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, json
-url = "http://localhost:3001/api/alerts"
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as res:
-    data = json.loads(res.read().decode())
-    for d in data:
-        if d['symbol'] == 'VEDL':
-            print(json.dumps(d, indent=2))
